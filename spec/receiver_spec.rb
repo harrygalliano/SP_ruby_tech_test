bd8577bb9ecb5775ef4781081044f91ed72d4a40
Harry Galliano <harrygalliano97@gmail.com>
2019-07-15 12:03:56 +0100

    added tests for unique views in the same format as most views

--- spec/receiver_spec.rb
+++ spec/receiver_spec.rb
@@ -23,4 +23,22 @@ describe Receiver do
                 expect(@receiver.most_views).to include(["/about/2", ["444.701.448.104"]])
             end
         end
+
+        describe 'unique views creates an array of data' do 
+            it 'displays the web pages with the most total views' do 
+                expect(@receiver.unique_views).not_to be_empty
+            end
+        end
+
+        describe 'unique views creates an array of data' do 
+            it 'displays the web pages with the most total views' do 
+               expect(@receiver.unique_views).to include(["/contact", ["184.123.665.067", "184.123.665.067"]])
+            end
+        end
+
+        describe 'unique views has a specified length' do 
+            it 'displays the web pages with the most total views' do 
+               expect(@receiver.unique_views.count).to eq(6)
+            end
+        end
     end
